a50348b9888598aded566a3f7dd2839c4e8e271c
DanielSchenki <k12006242@students.jku.at>
2023-04-20 22:04:34 +0200

    changed flask-app to real-time data-fetch

--- webservice/app.py
+++ webservice/app.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from flask import Flask, render_template
 import sqlite3
+import Adafruit_DHT
 
 app = Flask(__name__)
 
 
 # Retrieve data from database
-def get_data():
+def get_data() :
     time = ""
     temp, hum = (0, 0)
     conn = sqlite3.connect('../sensors_data.db')
@@ -20,10 +21,23 @@ def get_data():
         return time, temp, hum
 
 
+def get_live_data() :
+    hum, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+
+    if hum is not None and temp is not None :
+        temp = round(temp, 3)
+        hum = round(hum, 3)
+        time = datetime.now().strftime("%d.%m.%Y um %H:%M (live)")
+        return time, temp, hum
+
+    else:
+        return get_data()
+
+
 # main route
 @app.route("/")
 def index():
-    time, temp, hum = get_data()
+    time, temp, hum = get_live_data()
     template_data = {
         'time': time,
         'temp': temp,
