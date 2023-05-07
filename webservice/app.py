ea22c9cc70db2f94452f368c8ea27ac50d364a54
DanielSchenki <82052625+DanielSchenki@users.noreply.github.com>
2023-05-07 22:36:16 +0200

    removed live-data

--- webservice/app.py
+++ webservice/app.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template
 import sqlite3
-import Adafruit_DHT
 
 app = Flask(__name__)
 
@@ -21,23 +20,10 @@ def get_data() :
         return time, temp, hum
 
 
-def get_live_data() :
-    hum, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
-
-    if hum is not None and temp is not None :
-        temp = round(temp, 3)
-        hum = round(hum, 3)
-        time = datetime.now().strftime("%d.%m.%Y um %H:%M (live)")
-        return time, temp, hum
-
-    else:
-        return get_data()
-
-
 # main route
 @app.route("/")
 def index():
-    time, temp, hum = get_live_data()
+    time, temp, hum = get_data()
     template_data = {
         'time': time,
         'temp': temp,
